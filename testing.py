b5949b84131f03afc8898aa8d6729d0e65849992
rvrooij <rvrooij@gmail.com>
2020-01-29 16:21:01 +0100

    Added SUV analysis

--- testing.py
+++ testing.py
@@ -24,6 +24,23 @@ def loadData(dcm_folder):
     return data, pixsize
 
 
+def earlsuv_analysis(dcm_folder):    
+    ## TODO read these from config
+    
+
+    ## TODO read from manual input
+        
+    actual_concentration = 70E6/6250
+    
+    pixeldataIn, pixsize = loadData(dcm_folder)
+    
+    show = 1
+    margin_mm = 10
+    
+    measured_concentration = earllib.analyze_suv(pixeldataIn, pixsize, margin_mm, show)
+    print(measured_concentration)
+    
+
 def earliq_analysis(dcm_folder):    
     ## TODO read these from config
     spheres_ml = [26.42, 11.49, 5.57, 2.57, 1.15, 0.52]
@@ -57,7 +74,10 @@ def earliq_analysis(dcm_folder):
     plt.show()
     
 
+data = "Testdata/CALIBRATION QC/PETFDGSLACEARL20191114"
+earlsuv_analysis(data)
 
+exit()
 
 data = "Testdata/IMAGE QC/PETFDGSLACEARLIMAGEQC20191114"
 earliq_analysis(data)
